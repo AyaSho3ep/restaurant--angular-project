b99dc0d14eb8044d24178fef7519d63b6eae01ec
sho3ep <aya.sho3ep@gmail.com>
2022-04-07 01:05:38 +0200

    changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angularlab5-main'));
+app.use(express.static('./dist/my-app'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angularlab5-main/'}),
+    res.sendFile('index.html', {root: 'dist/my-app/'}),
 );
 
 // Start the app by listening on the default Heroku port
